6a517e05e3c8d081c2849a266b790d1d3fdc377b
Pervaz11 <parvazsa@code.edu.az>
2025-07-22 18:13:36 +0400

    feat: implemented language switch functionality using i18next

--- client/src/i18n/config.ts
+++ client/src/i18n/config.ts
@@ -1,35 +1,35 @@
-// import i18n from "i18next";
-// import { initReactI18next } from "react-i18next";
-// import LanguageDetector from "i18next-browser-languagedetector";
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
 
-// import en from "./locales/en/translation.json";
-// import az from "./locales/az/translation.json";
-// import tr from "./locales/tr/translation.json";
-// import ru from "./locales/ru/translation.json";
+import en from "./locales/en/translation.json";
+import az from "./locales/az/translation.json";
+import tr from "./locales/tr/translation.json";
+import ru from "./locales/ru/translation.json";
 
 
-// const resources = {
-//   en: { translation: en },
-//   az: { translation: az },
-//   tr: { translation: tr },
-//   ru: { translation: ru },
+const resources = {
+    en: { translation: en },
+    az: { translation: az },
+    tr: { translation: tr },
+    ru: { translation: ru },
 
-// };
+};
 
-// i18n
-//   .use(initReactI18next)
-//   .use(LanguageDetector)
-//   .init({
-//     resources: resources,
-//     fallbackLng: "en",
-//     interpolation: {
-//       escapeValue: false,
-//     },
-//     react: { useSuspense: false },
-//     detection: {
-//       order: ["localStorage", "navigator", "htmlTag"],
-//       caches: ["localStorage"],
-//     },
-//   });
+i18n
+    .use(initReactI18next)
+    .use(LanguageDetector)
+    .init({
+        resources: resources,
+        fallbackLng: "en",
+        interpolation: {
+            escapeValue: false,
+        },
+        react: { useSuspense: false },
+        detection: {
+            order: ["localStorage", "navigator", "htmlTag"],
+            caches: ["localStorage"],
+        },
+    });
 
-// export default i18n;
+export default i18n;
